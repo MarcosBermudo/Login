80a1e0aecec8465fdd8247e580e1dd52635ac7ee
marcos <mbermudo@itpfp.com>
2024-12-18 12:41:47 +0100

    bienvenido

--- Login/Form1.cs
+++ Login/Form1.cs
@@ -16,5 +16,11 @@ namespace Login
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.Show();
+        }
     }
 }
